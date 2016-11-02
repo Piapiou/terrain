4e27364c19acbface9e2c094ed170f61f8ea6db7
Tanguy Chanal <tanguy.chanal@gmail.com>
2016-11-02 09:04:59 +0100

    Update soil.cpp

--- soil.cpp
+++ soil.cpp
@@ -1,6 +1,22 @@
 #include "soil.h"
 
-Soil::Soil(float maxSoil)
+Soil::Soil(Terrain T, float maxSoil)
 {
+    this->T = T;
     this->maxSoil = maxSoil;
+}
+
+void Soil::setSoil(float x, float y) {
+
+    float zSoil = 0;
+    float zBed = T.getBedrockAt(x,y);
+
+    //calcul
+
+    if (zSoil > maxSoil){
+        zSoil = maxSoil;
+    }
+
+    T.setSoilAt(x,y,zSoil);
+
 }
