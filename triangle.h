3765ba74911a0bf1f209f75db7186b300f7cc643
Tanguy Chanal <tanguy.chanal@gmail.com>
2016-11-14 17:01:14 +0100

    Doc Triangle
    Documentation de la classe Triangle.

--- triangle.h
+++ triangle.h
@@ -4,18 +4,73 @@
 #include <QVector3D>
 #include <point.h>
 
+/**
+ * Représente un Triangle dans l'espace.
+ */
 class Triangle
 {
 public:
+
+    /**
+     * Instancie un Triangle dans l'espace.
+     */
     Triangle();
+
+    /**
+     * Instancie un Triangle dans l'espace selon trois indices.
+     * Chaque indice corrsepond à un point du vecteur de l'ensemble des points.
+     */
     Triangle(int i1, int i2, int i3);
+
+    /**
+     * Renvoit l'indice du premier sommet du triangle.
+     * L'indice correspond à un point du vecteur de l'ensemble des points.
+     */
     int x();
+
+    /**
+     * Renvoit l'indice du second sommet du triangle.
+     * L'indice correspond à un point du vecteur de l'ensemble des points.
+     */
     int y();
+
+    /**
+     * Renvoit l'indice du troisème sommet du triangle.
+     * L'indice correspond à un point du vecteur de l'ensemble des points.
+     */
     int z();
+
+    /**
+     * Définit l'égalité entre deux triangles.
+     */
     Triangle operator=(Triangle t);
+
+    /**
+     * Affecte l'indice du point opposé du premier triangle adjacent.
+     * L'indice doit correspondre à un point du vecteur de l'ensemble des points.
+     * @param i : Indice du point opposé
+     */
     void setAdj1(int i);
+
+    /**
+     * Affecte l'indice du point opposé du deuxième triangle adjacent.
+     * L'indice doit correspondre à un point du vecteur de l'ensemble des points.
+     * @param i : Indice du point opposé
+     */
     void setAdj2(int i);
+
+    /**
+     * Affecte l'indice du point opposé du troisième triangle adjacent.
+     * L'indice doit correspondre à un point du vecteur de l'ensemble des points.
+     * @param i : Indice du point opposé
+     */
     void setAdj3(int i);
+:
+    /**
+     * Retourne le point opposé d'un triangle adjacent
+     * @param nt : Indice du triangle adjacent
+     * @return L'indice du point opposé du triangle adjacent
+     */
     int getAdj(int nt);
 private:
     int p1, p2, p3;
