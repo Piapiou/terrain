ad9802b84cebff30fd35c375b860d7fe651c02f7
Tanguy Chanal <tanguy.chanal@gmail.com>
2016-11-16 16:56:29 +0100

    Doc ecosystem + corrections
    Documentation de la classe Ecosystem + Corrections mineures de la
    documentation.

--- ecosystem.h
+++ ecosystem.h
@@ -7,13 +7,57 @@
 
 class Terrain;
 
+/**
+ * Représente la couche de végétation du terrain.
+ */
 class Ecosystem
 {
 public:
+
+    /**
+     * Instancie une couche de végétation.
+     */
     Ecosystem();
+
+    /**
+     * Initialise la couche de végétation.
+     * Insère dans un vecteur d'arbres un certain nombre d'arbres à des positions aléatoires du terrain.
+     * Le vecteur d'arbres représente la couche de végétation du terrain.
+     * @param n : Nombre d'arbres initiaux.
+     * @param T : Terrain auquel correspond la couche de végétation.
+     */
     void init(int n, Terrain * T);
+
+    /**
+     * Incrémente le cycle de vie de la végétation.
+     * Fait progresser l'âge et le niveau de maladie de chaque arbre, et gère leur propagation et leur décès.
+     */
     void step();
+
+    /**
+     * Ajoute un arbre à la couche de végétation.
+     * L'arbre est inséré dans le vecteur d'arbre représentant la couche de végétation.
+     * La probabilité que l'arbre soit inséré dans le vecteur est égale à la probabilité d'existence de l'arbre à la position indiquée.
+     * Si l'arbre n'a pas la place d'exister à la position indiquée, il n'est pas inséré.
+     * @param x : Position en abscisse de l'arbre.
+     * @param y : Position en ordonnée de l'arbre.
+     * @param age : Âge de l'arbre.
+     * @param type : Type de l'arbre.
+     */
+    void plant(int x, int y, int age, int type);
+
+    /**
+     * Génère une image représentant la répartition de la végétation.
+     * Chaque type d'arbre est représenté sur la carte par une couleur différente.
+     * @param Chemin du dossier d'enregistrement de l'image.
+     */
     void exportImg(QString path);
+
+    /**
+     * Renvoie un objet Mesh dépendant du vecteur d'arbres.
+     * Chaque arbre du vecteur est converti en Mesh, et integré au Mesh gloal renvoyé.
+     * @return Un objet Mesh.
+     */
     Mesh generateMesh();
 
 private:
--- flowmap.h
+++ flowmap.h
@@ -14,14 +14,14 @@ public:
 
     /**
      * Instancie une carte d'écoulement des eaux.
-     * @param T : Le terrain auquel correspond la carte.
+     * @param T : Terrain auquel correspond la carte.
      * @param nbIteration : Nombre d'itérations sur lesquelles la carte est générée.
      */
     Flowmap(Terrain * T, int nbIteration);
 
     /**
      * Génère la carte d'écoulement du terrain.
-     * À chaque itération, est calculé pour chaque point du terrain le déplacement de l'eau, jusqu'à aboutir à l'emplacement final des cours d'eau.
+     * À chaque itération, le déplacement de l'eau est calculé pour chaque point du terrain, jusqu'à aboutir à l'emplacement final des cours d'eau.
      */
     void generateFlowMap();
 
